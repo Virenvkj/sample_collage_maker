7f8dbd73c4c863e32f5e4254a53c934018f0cdf8
Viren Gajjar <vgajjar1010@gmail.com>
2021-04-20 11:36:40 +0530

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_collage_maker/image_collage_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,36 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Sample Collage Maker'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    new MaterialPageRoute(
+                      builder: (context) => ImageCollageWidget(),
+                    ),
+                  );
+                },
+                child: Text('Image Collage Widget'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/image_collage_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImageCollageWidget extends StatefulWidget {
+  @override
+  _ImageCollageWidgetState createState() => _ImageCollageWidgetState();
+}
+
+class _ImageCollageWidgetState extends State<ImageCollageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
